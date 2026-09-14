fc202a6a7b9af782ee7b3e977340893080935b32
special <lyovinalexandr21@gmail.com>
2026-09-14 12:48:33 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+"""#1
+n=int(input(‘n=?’))
+k=int(input(‘k=?’))
+for i in range (0,n):
+print(k)"""
+
+"""#2
+a=int(input())
+b=int(input())
+n=0
+for i in range(a,b+1):
+n+=1
+print(i)
+print(n)"""
+
+"""#4
+a = int(input(‘цена: ‘))
+c = 0
+for i in range(1, 11):
+c += 1
+i *= a
+print(f’за {c}кг цена: {i}’)"""
